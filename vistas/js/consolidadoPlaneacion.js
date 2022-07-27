b0d07addd455bb415362f813d50e29bfbeabd965
JDavid80 <jdcalderong@gmail.com>
2022-07-27 10:19:21 -0500

    Pruebas Columnas consolidado!

--- vistas/js/consolidadoPlaneacion.js
+++ vistas/js/consolidadoPlaneacion.js
@@ -10,24 +10,28 @@ function listarPlaneaciones() {
             })
             .then(res => res.json())
             .then(data => {
-                console.log(data);
                 
                 data.forEach(element => {
-                    /* tabla+=`<tr><td>${element.nombre_proceso}</td>
-                    <td>${element.nombres_orientador} ${element.primer_apellido}</td>
-                    </tr>` */
                     tabla += `<tr>
                                 <td>${element.nombre_curso}</td>
                                 <td>${element.nombre_proceso}</td>
                                 <td>${element.nombres_orientador} ${element.primer_apellido}</td>
                                 <td>${element.nombre_periodo}</td>
                                 <td>${element.meta_comprension_micro}</td>
+                                <td><div class="div-iconos"><i id="pruebaDiv" class='iconos fa-regular fa-file-lines'></i></div></td>
+                                <td><div class="div-iconos"><i class='iconos fa-solid fa-print'></i></div></td>
                     </tr>`
+                    
                 });
                 
                document.getElementById('tabla').innerHTML=tabla;
             });
           
+    let pruebaDiv = document.getElementById("PruebaDiv");
+
+    pruebaDiv.addEventListener("click", function() {
+        alert("Holis");
+    });
        
     
 }
